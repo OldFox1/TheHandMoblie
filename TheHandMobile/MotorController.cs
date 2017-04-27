058de0b12f79b1d195fdc006181f468a0daf756a
OldFox1 <Barakh1990@gmail.com>
2017-04-27 22:58:30 +0300

    back the myo code
    back the myo code

---
+++ TheHandMobile/MotorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+
+namespace TheHandMobile
+{
+    class MotorController
+    {
+
+        private readonly RestClient client;
+
+        public MotorController()
+        {
+            client = new RestClient("http://10.10.0.252/");
+
+        }
+
+        public void moveForward()
+        {
+            sendCommand("forward");
+        }
+
+        public void reverse()
+        {
+            sendCommand("reverse");
+        }
+
+        public void turnRight()
+        {
+            sendCommand("right");
+        }
+
+        public void turnLeft()
+        {
+            sendCommand("left");
+        }
+
+        private void sendCommand(string resourceName)
+        {
+
+            var request = new RestRequest(resourceName, Method.POST);
+            client.Execute(request);
+
+        }
+
+    }
+}
